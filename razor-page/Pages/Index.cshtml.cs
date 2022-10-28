e50e7cebbed8d5d97483132e5d56e05092315b19
phantuan2605 <113750741+phantuan2605@users.noreply.github.com>
2022-10-28 11:15:56 +0700

    Add files via upload

---
+++ razor-page/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ajax.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+    
+    public IActionResult OnPostGetAjax(string name)
+    {
+        return new JsonResult("Hello " + name);
+    }
+}
---
+++ razor-page/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$("#callAjax").click(function () {
+	$.ajax({
+		type: "POST",
+		url: "/Index?handler=GetAjax",
+		data: { "name": "Mark" },
+		contentType: 'application/x-www-form-urlencoded',
+		dataType: "json",
+		headers:
+		{
+			"RequestVerificationToken": $('input:hidden[name="__RequestVerificationToken"]').val()
+		},
+		success: function (msg) {
+			alert(msg);
+		}
+	});
+});
